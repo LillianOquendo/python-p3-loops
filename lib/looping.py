12641a66c34c8786115204633bdb7eba87b20b00
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-06-15 22:21:15 -0400

    Python Loops Lab Complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > -1:
+        if i > 0:
+            print(i)
+        else:
+            print ("Happy New Year!")
+        i -= 1
+        
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [num * num for num in int_list]
+    return square_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # i = 1
+    # while i <= 100:
+    #     if (i % 3 == 0 and i % 5 == 0):
+    #         print ("FizzBuzz")
+    #     elif (i % 3 == 0):
+    #         print ("Fizz")
+    #     elif (i % 5 == 0):
+    #         print ("Buzz")
+    #     else:
+    #         print (i)
+    #     i += 1
+
+    for i in range (1, 101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print ("FizzBuzz")
+        elif (i % 3 == 0):
+            print ("Fizz")
+        elif (i % 5 == 0):
+            print ("Buzz")
+        else:
+            print (i)
